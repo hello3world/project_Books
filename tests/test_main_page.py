f3b3fbc10bf5546bdf940ea96d4293eda0d0703c
Yauheni Paulovich <e.pavlovich29@gmail.com>
2024-10-19 15:46:41 +0300

    feat: add (def should_be_login_link) in test_main_page.py

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -10,3 +10,9 @@ def test_guest_can_go_to_login_page(browser):
     # page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
     time.sleep(5)
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
