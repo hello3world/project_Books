bf7826a30e6bf2833313ced98d77e42f84be994a
Yauheni Paulovich <e.pavlovich29@gmail.com>
2024-10-19 17:13:58 +0300

    feat: add login_page.py with 3 methods should_be

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    REGISTER_FORM_LINK = (By.XPATH, "//h2[text()='Войти']")
+    LOGIN_FORM_LINK = (By.XPATH, "//h2[text()='Зарегистрироваться']")
---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.browser.current_url == 'https://selenium1py.pythonanywhere.com/ru/accounts/login/', 'current login is not equal'
+
+    def should_be_login_form(self):
+        self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login link is not presented"
+
+    def should_be_register_form(self):
+        self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM_LINK), "Login link is not presented"
